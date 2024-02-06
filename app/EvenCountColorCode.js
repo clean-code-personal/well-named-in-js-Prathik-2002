523bde46a2859c4ec9cac4d38747289b341fde32
Prathik-2002 <acharyaprathik4@gmail.com>
2024-02-06 11:20:16 +0530

    variableName change

--- app/EvenCountColorCode.js
+++ app/EvenCountColorCode.js
@@ -6,23 +6,23 @@ export class EvenCountColorCode {
     MinorColorNames = [
         "BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"
     ];
-    COLOR_PAIRS = {}
-    COLOR_PAIRS_KEY_VALUE_REVERSED = {}
+    PAIRNUMBER_COLORPAIR_MAP = {}
+    COLORPAIR_PAIRNUMBER_MAP = {}
 
     constructor(){
         let minorColorSize = this.MinorColorNames.length
         let majorColorSize = this.MajorColorNames.length
         for (let i = 0; i < majorColorSize; i++){
             for (let j = 0; j < minorColorSize; j++){
-                this.COLOR_PAIRS[(i*majorColorSize)+(j + 1)] = [this.MajorColorNames[i],this.MinorColorNames[j]]
-                this.COLOR_PAIRS_KEY_VALUE_REVERSED[[this.MajorColorNames[i],this.MinorColorNames[j]]] = (i*majorColorSize)+(j + 1)
+                this.PAIRNUMBER_COLORPAIR_MAP[(i*majorColorSize)+(j + 1)] = [this.MajorColorNames[i],this.MinorColorNames[j]]
+                this.COLORPAIR_PAIRNUMBER_MAP[[this.MajorColorNames[i],this.MinorColorNames[j]]] = (i*majorColorSize)+(j + 1)
             }
         }
     }
     getManual = ()=>{
         let manualTemplate = "COLOR PAIR - MANUAL\n";
-        Object.keys(this.COLOR_PAIRS).forEach(key => {
-            manualTemplate += `Pair No: ${key} => [MajorColor: ${this.COLOR_PAIRS[key][0]}, MinorColor: ${this.COLOR_PAIRS[key][1]}]\n`
+        Object.keys(this.PAIRNUMBER_COLORPAIR_MAP).forEach(key => {
+            manualTemplate += `Pair No: ${key} => [MajorColor: ${this.PAIRNUMBER_COLORPAIR_MAP[key][0]}, MinorColor: ${this.PAIRNUMBER_COLORPAIR_MAP[key][1]}]\n`
         })
         return manualTemplate
     };
@@ -30,15 +30,15 @@ export class EvenCountColorCode {
         if(pairNumber>(this.MajorColorNames.length*this.MinorColorNames.length) || pairNumber<1) {
             throw `Argument PairNumber:${pairNumber} is outside the allowed range`
         }
-        const pair_in_list = this.COLOR_PAIRS[pairNumber]
+        const pair_in_list = this.PAIRNUMBER_COLORPAIR_MAP[pairNumber]
         return new ColorPair(pair_in_list[0], pair_in_list[1])
     }
     getPairNumberFromColor = (pair) => {
-        const pairnum = this.COLOR_PAIRS_KEY_VALUE_REVERSED[[pair.majorColor,pair.minorColor]]
+        const pairnum = this.COLORPAIR_PAIRNUMBER_MAP[[pair.majorColor,pair.minorColor]]
         if(pairnum == undefined){
             throw `${pair} combination does not exist`
         }
         else
-        return this.COLOR_PAIRS_KEY_VALUE_REVERSED[[pair.majorColor,pair.minorColor]]
+        return this.COLORPAIR_PAIRNUMBER_MAP[[pair.majorColor,pair.minorColor]]
     }
 }
